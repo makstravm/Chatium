3b66052d7f50b405d0f7fd734af51fd5ea9a979e
makstravm <maks7294@gmail.com>
2022-09-20 00:10:31 +0300

    finished config huski

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Ass = () => {
-  console.log("asd");
-
-  return (
-    <span>
-      Hi
-      <span>{}</span>
-    </span>
-  );
-};
-
-ReactDOM.render(
-  <div>
-    <Ass />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<div>hi</div>, document.getElementById("root"));
