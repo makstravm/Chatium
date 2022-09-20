e5e9ae82eed2abd96a447af48a5098260292f03e
makstravm <maks7294@gmail.com>
2022-09-20 13:48:39 +0300

    added compionent App

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
-ReactDOM.render(<div>hi</div>, document.getElementById("root"));
+render(<div>hi</div>, document.getElementById("root"));
