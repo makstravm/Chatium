cc0bafb3777a3013b4d75ed325fb046d37dd9a34
makstravm <maks7294@gmail.com>
2022-09-21 11:41:42 +0300

    added redux/toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
 import "./style.scss";
 
-render(<App />, document.getElementById("root"));
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
