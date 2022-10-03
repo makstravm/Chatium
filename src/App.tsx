b7ce5cb3e52f27dbf9f983c7cca3d8d8dda8e933
makstravm <maks7294@gmail.com>
2022-10-03 16:21:14 +0300

    removed "import React" in all files, changed tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SignIn from "components/Authentication/SignIn";
--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 
 import { Box, Container, Typography } from "@mui/material";
