65cd3d597b97c01d5b518210a3554c55d752e098
makstravm <maks7294@gmail.com>
2022-10-03 14:51:01 +0300

    fixed password check so that users understand what exactly they enterd incorrectly

--- src/helpers/loginValidationSchema/index.ts
+++ src/helpers/loginValidationSchema/index.ts
@@ -6,9 +6,23 @@ export const loginValidationSchema = Yup.object().shape({
     .trim("Field cann't contain spaces")
     .required("Field required"),
   password: Yup.string()
+    .required("Field required")
     .trim("Field cann't contain spaces")
-    .matches(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
-      message: "Password is incorrect",
-    })
-    .required("Field required"),
+    .min(6, "Password has to be longer than 6 characters!")
+    .matches(/[a-z]+/, "password must contain at least 1 lower case letter")
+    .matches(/[A-Z]+/, "password must contain at least 1 upper case letter")
+    .matches(/\d+/, "password must contain at least 1 number")
+    .test("not special symbols", function array(value) {
+      const checkSymbolInPassword =
+        /^.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?].*$/.test(value as string);
+
+      if (checkSymbolInPassword) {
+        throw this.createError({
+          path: this.path,
+          message: "must not contain special characters",
+        });
+      }
+
+      return true;
+    }),
 });
