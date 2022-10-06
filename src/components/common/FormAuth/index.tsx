2a07e91d438a7a0ef9412c1dd2bc0e6b173905e6
makstravm <maks7294@gmail.com>
2022-10-06 17:09:30 +0300

    added translate for components

--- src/components/common/FormAuth/index.tsx
+++ src/components/common/FormAuth/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { Form, Formik, FormikProps } from "formik";
 import { FirebaseError } from "firebase/app";
 import {
@@ -29,102 +30,108 @@ const FormAuth: FC<IFormProps> = ({
   isLoading,
   errorMessage,
   isError,
-}) => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    onSubmit={(values) => onSubmit(values)}
-  >
-    {({
-      errors,
-      touched,
-      handleChange,
-      isValid,
-      dirty,
-    }: FormikProps<FormikValuesType>) => (
-      <Box
-        sx={{
-          pt: 4,
-          borderRadius: "15px",
-          boxShadow: `0 0 10px 0px ${!isError ? "#404040" : "#c51111"}`,
-        }}
-      >
-        <Form>
-          <Grid container justifyContent="center" spacing={2}>
-            {isError && (
-              <Grid item xs={10}>
-                <Alert variant="outlined" severity="error">
-                  <div className={style.errorMessage}>
-                    {handlerErrorMessage(errorMessage as FirebaseError)}
-                  </div>
-                </Alert>
-              </Grid>
-            )}
-            {formFields.map(({ id, name, type, label }) => (
-              <Grid key={id} item xs={10}>
-                {type === TEXT ? (
-                  <TextField
-                    name={name}
-                    label={label}
-                    type={type}
-                    size="small"
-                    fullWidth
-                    error={!!(touched[name] && errors[name])}
-                    helperText={touched[name] && errors[name]}
-                    onChange={handleChange}
-                  />
-                ) : (
-                  <PasswordField
-                    name={name}
-                    label={label}
-                    type={type}
-                    errors={errors}
-                    touched={touched}
-                    handleChange={handleChange}
-                  />
-                )}
-              </Grid>
-            ))}
-            {labelCheckBox && (
-              <Grid item xs={10}>
-                <FormControlLabel
-                  control={<Checkbox name="checkbox" onChange={handleChange} />}
-                  label={labelCheckBox}
-                  componentsProps={{
-                    typography: {
-                      fontSize: ".9em",
-                    },
-                  }}
-                />
-              </Grid>
-            )}
-          </Grid>
-          <Box
-            sx={{
-              mt: 2,
-              pb: 2,
-            }}
-            textAlign="center"
-          >
-            <Button
-              variant="outlined"
-              type="submit"
-              sx={{
-                minWidth: "86px",
-              }}
-              disabled={isLoading || (!isValid && !dirty)}
-            >
-              {!isLoading ? (
-                buttonTitle
-              ) : (
-                <CircularProgress color="inherit" size="1.5rem" />
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => onSubmit(values)}
+    >
+      {({
+        errors,
+        touched,
+        handleChange,
+        isValid,
+        dirty,
+      }: FormikProps<FormikValuesType>) => (
+        <Box
+          sx={{
+            pt: 4,
+            borderRadius: "15px",
+            boxShadow: `0 0 10px 0px ${!isError ? "#404040" : "#c51111"}`,
+          }}
+        >
+          <Form>
+            <Grid container justifyContent="center" spacing={2}>
+              {isError && (
+                <Grid item xs={10}>
+                  <Alert variant="outlined" severity="error">
+                    <div className={style.errorMessage}>
+                      {handlerErrorMessage(errorMessage as FirebaseError)}
+                    </div>
+                  </Alert>
+                </Grid>
               )}
-            </Button>
-          </Box>
-        </Form>
-      </Box>
-    )}
-  </Formik>
-);
+              {formFields.map(({ id, name, type }) => (
+                <Grid key={id} item xs={10}>
+                  {type === TEXT ? (
+                    <TextField
+                      name={name}
+                      label={t(`auth.formLabels.${name}`)}
+                      type={type}
+                      size="small"
+                      fullWidth
+                      error={!!(touched[name] && errors[name])}
+                      helperText={touched[name] && errors[name]}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <PasswordField
+                      name={name}
+                      label={t(`auth.formLabels.${name}`)}
+                      type={type}
+                      errors={errors}
+                      touched={touched}
+                      handleChange={handleChange}
+                    />
+                  )}
+                </Grid>
+              ))}
+              {labelCheckBox && (
+                <Grid item xs={10}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox name="checkbox" onChange={handleChange} />
+                    }
+                    label={labelCheckBox}
+                    componentsProps={{
+                      typography: {
+                        fontSize: ".9em",
+                      },
+                    }}
+                  />
+                </Grid>
+              )}
+            </Grid>
+            <Box
+              sx={{
+                mt: 2,
+                pb: 2,
+              }}
+              textAlign="center"
+            >
+              <Button
+                variant="outlined"
+                type="submit"
+                sx={{
+                  minWidth: "86px",
+                }}
+                disabled={isLoading || (!isValid && !dirty)}
+              >
+                {!isLoading ? (
+                  buttonTitle
+                ) : (
+                  <CircularProgress color="inherit" size="1.5rem" />
+                )}
+              </Button>
+            </Box>
+          </Form>
+        </Box>
+      )}
+    </Formik>
+  );
+};
 
 export default FormAuth;
--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -1,19 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import { Box, Container, Typography } from "@mui/material";
 import logo from "assets/logo.png";
 
-const Authorization = () => (
-  <Box sx={{ display: "flex", alignItems: "center", minHeight: "100vh" }}>
-    <Container maxWidth="xs">
-      <Box sx={{ p: 2, pt: 3, display: "flex", justifyContent: "center" }}>
-        <img src={logo} alt="logo" width="100" />
-      </Box>
-      <Outlet />
-      <Typography variant="body2" align="center" pt={2} pb={2}>
-        © 2022 Chatium. Produced by Ukraine
-      </Typography>
-    </Container>
-  </Box>
-);
+const Authorization = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", minHeight: "100vh" }}>
+      <Container maxWidth="xs">
+        <Box sx={{ p: 2, pt: 3, display: "flex", justifyContent: "center" }}>
+          <img src={logo} alt="logo" width="100" />
+        </Box>
+        <Outlet />
+        <Typography variant="body2" align="center" pt={2} pb={2}>
+          © 2022 Chatium. {t("auth.footerText")}
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
 
 export default Authorization;
