d7e467e1139354c0fffffd14a1e0ec6cc9f1b7c0
makstravm <maks7294@gmail.com>
2022-10-21 12:06:20 +0300

    changed the way export in the types/indes.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,11 @@
-export {
-  IFormField,
-  ILoginInitialValue,
-  IRegistrationInitialValue,
-} from "types/formConstants";
+export * from "types/formConstants";
 
-export {
-  InitialValuesFormType,
-  ValidationSchemaTypes,
-  IFormProps,
-  FormikValuesType,
-  FormikSubmitType,
-} from "types/formAuth";
+export * from "types/formAuth";
 
-export {
-  ILoginValidationSchema,
-  IRegisterValidationSchema,
-} from "types/helpers";
+export * from "types/helpers";
 
-export { IThemeContext, IAuthContext } from "types/context";
+export * from "types/context";
 
-export { IPasswordField } from "types/passwordField";
+export * from "types/passwordField";
 
-export { IErrorMessage } from "types/errorMessage";
+export * from "types/errorMessage";
