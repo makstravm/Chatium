a9ee6bfff1bbabd90ffb2228ed97a7b86fe5b887
makstravm <maks7294@gmail.com>
2022-11-04 17:34:08 +0200

    changed IResponseSignIn

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -12,12 +12,9 @@ export interface IRequestAuthValues {
 }
 
 export interface IResponseSignIn {
-  kind: string;
   localId: string;
   email: string;
   displayName: string;
   idToken: string;
-  registered: boolean;
   refreshToken: string;
-  expiresIn: string;
 }
