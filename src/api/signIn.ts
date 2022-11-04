19b078db51bcae40103bf76ad700f75730b6f74a
makstravm <maks7294@gmail.com>
2022-11-04 12:47:30 +0200

    put link for request auth in the .env

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -3,7 +3,7 @@ import { POST } from "services/HTTPService";
 
 export const signInApi = async <T, V>(value: V): Promise<AxiosResponse<T>> => {
   const result = await POST<T, V>(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    `${process.env.FIREBASE_AUTH_API}signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
     value
   );
 
