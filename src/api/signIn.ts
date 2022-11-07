9b2a0a69a113f2d16d1ce845bc60b2aa882ff946
makstravm <maks7294@gmail.com>
2022-11-07 13:02:46 +0200

    changed name from  FIREBASE_AUTH_API to FIREBASE_AUTH_API_PATH

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -6,7 +6,7 @@ export const signInApi = async (
   value: IRequestAuthValues
 ): Promise<AxiosResponse<IResponseSignIn>> => {
   const result = await POST<IResponseSignIn, IRequestAuthValues>(
-    `${process.env.FIREBASE_AUTH_API}signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    `${process.env.FIREBASE_AUTH_API_PATH}signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
     value
   );
 
