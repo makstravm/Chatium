73e7c1ace9d1b561b40491e9418f2f5631f64d20
makstravm <maks7294@gmail.com>
2022-11-14 13:26:41 +0200

    added rules in the .eslint.json, removed async await from function signInApi

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -1,10 +1,10 @@
 import { POST } from "services/HTTPService";
 import { IRequestAuthValues, IResponseSignIn } from "types";
 
-export const signInApi = async (
+export const signInApi = (
   value: Omit<IRequestAuthValues, "returnSecureToken">
 ) =>
-  await POST<IResponseSignIn, IRequestAuthValues>(
+  POST<IResponseSignIn, IRequestAuthValues>(
     `${process.env.FIREBASE_AUTH_API_PATH}signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
     { ...value, returnSecureToken: true }
   );
